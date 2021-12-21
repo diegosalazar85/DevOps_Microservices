9d92d7b2aab3e43778938335dcab0c4af2a793cf
Diego Salazar <diego.salazar@gorillalogic.com>
2021-12-21 15:30:11 -0600

    Run project locally, in Docker and in K8s

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="diegosalazar85/project-ml-microservices-kubernetes"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run project-ml-microservices-kubernetes\
+    --image=$dockerpath\
+    --port=80 --labels app=project-ml-microservices-kubernetes
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
-# Forward the container port to a host
-
+# Forward the container port to a hos
+kubectl port-forward project-ml-microservices-kubernetes 8000:80
